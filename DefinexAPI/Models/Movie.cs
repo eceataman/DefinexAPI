91e8e1f2f16f664dfec5277aa93a68674b824926
Ece Ataman <114909014+eceataman@users.noreply.github.com>
2025-03-10 23:47:39 +0300

    Add project files.

---
+++ DefinexAPI/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace DefinexAPI.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ DefinexAPI/Models/MovieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DefinexAPI.Models
+{
+    public class MovieDbContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movie");
+        }
+    }
+}
