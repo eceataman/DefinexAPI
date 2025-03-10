803c6ab13bd69dfe4e96c48e4baf7ff6529cc47a
Ece Ataman <114909014+eceataman@users.noreply.github.com>
2025-03-11 00:08:41 +0300

    get ile veriler alındı

---
+++ DefinexAPI/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using DefinexAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DefinexAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MovieDbContext _context;
+        public MovieController(MovieDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+    }
+}
